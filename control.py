50685f6fd225fb481035a1a048ef164d43e228ec
namsan <namsan@kaist.ac.kr>
2018-01-06 00:49:03 +0100

    Implement Temporal Difference control and its basic test

--- control.py
+++ control.py
@@ -44,3 +44,52 @@ class MCControl:
             self.Qsa[state, action] += alpha * (G - self.Qsa[state, action])
 
         self.episode = []
+
+
+class TDControl:
+    def __init__(self, num_state, num_action, gamma=1, n0=100, lambda_val=0):
+        self.GAMMA = gamma
+        self.N0 = n0
+        self.LAMBDA = lambda_val
+
+        self.Qsa = np.zeros((num_state, num_action))
+        self.Esa = np.zeros((num_state, num_action))
+        self.Ns = np.zeros((num_state,))
+        self.Nsa = np.zeros((num_state, num_action))
+
+        self.prev_exp = []
+
+    def action(self, state):
+        epsilon = self.N0 / (self.N0 + self.Ns[state])
+        return epsilon_greedy(epsilon, self.Qsa[state])
+
+    def experience(self, state, action, reward):
+        if self.prev_exp:
+            prev_state = self.prev_exp[0]
+            prev_action = self.prev_exp[1]
+            prev_reward = self.prev_exp[2]
+
+            # exception : N(s, a) is zero
+            alpha_sa = 1 / np.maximum(self.Nsa, np.ones(self.Nsa.shape))
+            target_err = prev_reward + self.GAMMA*self.Qsa[state, action] - self.Qsa[prev_state, prev_action]
+            self.Qsa += target_err * np.multiply(alpha_sa, self.Esa)
+            self.Esa *= self.GAMMA * self.LAMBDA
+
+        self.prev_exp = [state, action, reward]
+        self.Esa[state, action] += 1
+        self.Ns[state] += 1
+        self.Nsa[state, action] += 1
+
+    def finish_episode(self):
+        prev_state = self.prev_exp[0]
+        prev_action = self.prev_exp[1]
+        prev_reward = self.prev_exp[2]
+
+        # exception : N(s, a) is zero
+        alpha_sa = 1 / np.maximum(self.Nsa, np.ones(self.Nsa.shape))
+        # Q(terminated, a) = 0
+        target_err = prev_reward - self.Qsa[prev_state, prev_action]
+        self.Qsa += target_err * np.multiply(alpha_sa, self.Esa)
+
+        self.prev_exp = []
+        self.Esa.fill(0)
--- test/test_control.py
+++ test/test_control.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from control import epsilon_greedy, MCControl
+from control import epsilon_greedy, MCControl, TDControl
 
 
 class TestEpsilonGreedy(TestCase):
@@ -17,3 +17,22 @@ class TestMCControl(TestCase):
 
         self.assertEqual(control.Qsa[0, 0], 10)
         self.assertEqual(control.Nsa[0, 0], 1)
+
+
+class TestTDControl(TestCase):
+    def test_control(self):
+        control = TDControl(10, 7)
+        control.experience(0, 0, 10)
+        control.finish_episode()
+
+        self.assertEqual(control.Qsa[0, 0], 10)
+        self.assertEqual(control.Nsa[0, 0], 1)
+
+        control = TDControl(10, 7, lambda_val=1)
+        control.experience(0, 0, 10)
+        control.experience(1, 0, 20)
+        control.finish_episode()
+
+        print(control.Qsa[0, 0])
+        self.assertEqual(control.Qsa[0, 0], 30)
+        self.assertEqual(control.Qsa[1, 0], 20)
